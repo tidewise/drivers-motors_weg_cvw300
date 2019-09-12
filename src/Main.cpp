a984cc080c4a59fbcca457fbd4c16a4b99ae5b29
Sylvain <sylvain.joyeux@tidewise.io>
2019-09-12 11:00:30 -0300

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <motors_weg_cvw300/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    motors_weg_cvw300::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
